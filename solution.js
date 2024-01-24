aaa62bac2db13bb67d94f58216a2d91e2ba3bbc4
Daniel Samuel <dansam5000@gmail.com>
2024-01-24 20:16:46 +0100

    Implement exercise solution

---
+++ solution.js
@@ -0,0 +1,21 @@
+// Write a constructor for making “Book” objects. We will revisit this in the project at the end of this lesson. Your book objects should have the book’s title, author, the number of pages, and whether or not you have read the book.
+
+// Put a function into the constructor that can report the book info like so:
+
+// theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+// Note: It is almost always best to return things rather than putting console.log() directly into the function. In this case, return the info string and log it after the function has been called:
+
+// console.log(theHobbit.info());
+
+function Book(title, author, pageNumber) {
+  this.title = title;
+  this.author = author;
+  this.pageNumber = pageNumber;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pageNumber} pages, not read yet`;
+  };
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295);
+
+console.log(theHobbit.info());
